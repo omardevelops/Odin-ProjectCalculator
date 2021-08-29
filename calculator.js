c739cadb59eb0456635f0949d0a187d2ad1fa2f0
Omar Ahmad <superomar121@gmail.com>
2021-08-29 13:47:22 +0400

    init

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+function operate(operator, num1, num2) {
+    
+}
