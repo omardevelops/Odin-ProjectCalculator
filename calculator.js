4c105e2102141d987093afcc1fb2e59266b09c80
Omar Ahmad <superomar121@gmail.com>
2021-08-29 15:59:47 +0400

    feat: add operate function

--- calculator.js
+++ calculator.js
@@ -13,6 +13,18 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
 function operate(operator, num1, num2) {
-    
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return 'I AM ERROR.';
+    }
 }
+
