8ee73ef58de69ec1d29edecdb1c8a5acd2282284
Omar Ahmad <superomar121@gmail.com>
2021-10-29 20:35:37 +0400

    added: display updates to pressing number buttons

--- calculator.js
+++ calculator.js
@@ -28,3 +28,55 @@ function operate(operator, num1, num2) {
     }
 }
 
+function isOperatorIncluded(text) {
+    return displayText.includes('+') || displayText.includes('-') || displayText.includes('×') || displayText.includes('÷');
+}
+
+function updateDisplay(text) {
+    display.textContent = text;
+}
+
+const button_container = document.querySelector('#buttons-cluster');
+const number_buttons = button_container.getElementsByClassName('number');
+const operator_buttons = button_container.getElementsByClassName('operator');
+const clear_button = button_container.querySelector('#clear');
+const decimal_button = button_container.querySelector('#decimal');
+const display = document.querySelector('#display');
+
+let displayText = 0;
+let operatorFlag = false; // Used to prevent invalid duplicate operators
+
+
+Array.from(number_buttons).forEach(function (button) {
+    button.addEventListener('click', function () {
+        if (display.textContent === '0') {
+            updateDisplay(button.textContent);
+        } else {
+            updateDisplay(display.textContent + button.textContent);
+        }
+        operatorFlag = false;
+    });
+});
+
+Array.from(operator_buttons).forEach(function (button) {
+    button.addEventListener('click', function () {
+        if (!operatorFlag && displayText.length != 0) {
+            displayText = displayText + button.textContent;
+            operatorFlag = true;
+        } else {
+            if (button.id === 'subtract' && displayText.charAt(displayText.length - 1) !== '−') {
+                displayText = displayText + button.textContent;
+            }
+        }
+
+        console.log(displayText);
+    })
+})
+
+clear_button.addEventListener('click', function () {
+    display.textContent = '0';
+});
+
+decimal_button.addEventListener('click', function () {
+    console.log(decimal_button.textContent);
+});
