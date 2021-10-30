90a665200dca4bde36420fe779b5c6a245a1be38
Omar Ahmad <superomar121@gmail.com>
2021-10-30 23:46:11 +0400

    added: operator capabilities

--- calculator.js
+++ calculator.js
@@ -15,13 +15,13 @@ function divide(x, y) {
 }
 
 function operate(operator, num1, num2) {
-    if (operator === '+') {
+    if (operator === 'add') {
         return add(num1, num2);
-    } else if (operator === '-') {
+    } else if (operator === 'subtract') {
         return subtract(num1, num2);
-    } else if (operator === '*') {
+    } else if (operator === 'multiply') {
         return multiply(num1, num2);
-    } else if (operator === '/') {
+    } else if (operator === 'divide') {
         return divide(num1, num2);
     } else {
         return 'I AM ERROR.';
@@ -36,47 +36,75 @@ function updateDisplay(text) {
     display.textContent = text;
 }
 
+function calculateResult(operator, num1, num2) {
+    console.group([operator, num1, num2]);
+    updateDisplay(operate(operator, num1, num2));
+    Array.from(operator_buttons).forEach(function (button) {
+        button.classList.remove('operator-pressed');
+    });
+    // operatorFlag = false;
+}
+
 const button_container = document.querySelector('#buttons-cluster');
 const number_buttons = button_container.getElementsByClassName('number');
 const operator_buttons = button_container.getElementsByClassName('operator');
 const clear_button = button_container.querySelector('#clear');
 const decimal_button = button_container.querySelector('#decimal');
+const equal_button = button_container.querySelector('#equal');
 const display = document.querySelector('#display');
 
 let displayText = 0;
-let operatorFlag = false; // Used to prevent invalid duplicate operators
+let heldNumber, newNumber, operator;
+let operatorFlag = false; // True means clicked on operator
 
 
 Array.from(number_buttons).forEach(function (button) {
     button.addEventListener('click', function () {
-        if (display.textContent === '0') {
-            updateDisplay(button.textContent);
+        if (operatorFlag === false) {
+            if (display.textContent === '0') {
+                updateDisplay(button.textContent);
+            } else {
+                updateDisplay(display.textContent + button.textContent);
+            }
         } else {
-            updateDisplay(display.textContent + button.textContent);
+            heldNumber = display.textContent
+            console.log(display.textContent);
+            display.textContent = '';
+            newNumber = display.textContent + button.textContent;
+            updateDisplay(newNumber);
+            // operate(operator, heldNumber, newNumber)
         }
-        operatorFlag = false;
+        
     });
 });
 
 Array.from(operator_buttons).forEach(function (button) {
     button.addEventListener('click', function () {
-        if (!operatorFlag && displayText.length != 0) {
-            displayText = displayText + button.textContent;
-            operatorFlag = true;
-        } else {
-            if (button.id === 'subtract' && displayText.charAt(displayText.length - 1) !== '−') {
-                displayText = displayText + button.textContent;
-            }
-        }
-
-        console.log(displayText);
+        console.log('hello');
+        operatorFlag = true;
+        operator = button.id;
+        
+        button.classList.add('operator-pressed');
+        console.log(button);
+        Array.from(operator_buttons).forEach(function (btn) {
+            if (btn.id != button.id) btn.classList.remove('operator-pressed');
+            console.log(btn);
+        });
     })
+});
+
+equal_button.addEventListener('click', function () {
+    calculateResult(operator, parseFloat(heldNumber), parseFloat(newNumber));
 })
 
 clear_button.addEventListener('click', function () {
     display.textContent = '0';
+    operatorFlag = false;
+    Array.from(operator_buttons).forEach(function (button) {
+        button.classList.remove('operator-pressed');
+    });
 });
 
-decimal_button.addEventListener('click', function () {
-    console.log(decimal_button.textContent);
-});
+// decimal_button.addEventListener('click', function () {
+//     console.log(decimal_button.textContent);
+// });
